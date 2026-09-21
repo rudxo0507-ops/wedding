733b7f517436c8ccd9a5b5f06513d6f6a917e807
rudxo0507-ops <rudxo0507@gmail.com>
2026-09-21 17:36:54 +0900

    Connect wedding photo share to new Apps Script backend

--- photo-share/config.js
+++ photo-share/config.js
@@ -1,5 +1,5 @@
 window.WEDDING_CONFIG = {
-  backendUrl: "https://script.google.com/macros/s/AKfycbxZu4aSkC-ZbR8fXXQBp_kPj-poppv8Eso3OcsN4-7_o1esEVLS1PgzzA5y_Zsco3nidg/exec",
+  backendUrl: "https://script.google.com/macros/s/AKfycby67kIEFVO3HlXHKKjNIS9vEz8CE7c8OBBOeNHfY3odIHB0aQwcmofH28-Kq5HmVHTh5A/exec",
   groom: "고경환",
   bride: "박수진",
   date: "2026. 10. 17",
